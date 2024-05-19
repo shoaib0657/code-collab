ed9cb69ee7361e6fa58a33389996d75bdf39a11c
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2024-05-20 00:53:54 +0530

    Add functionality to create a new room on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 const Home = () => {
+
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
+  const createNewRoom = (e) => {
+    e.preventDefault()
+    const id = uuid();
+    setRoomId(id);
+  }
+
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
@@ -8,11 +19,11 @@ const Home = () => {
         <h4 className='mainLabel'>Paste invitation ROOM ID</h4>
 
         <div className='inputGroup'>
-            <input type='text' className='inputBox' placeholder='ROOM ID' />
-            <input type='text' className='inputBox' placeholder='USERNAME' ></input>
+            <input type='text' className='inputBox' placeholder='ROOM ID' onChange={(e) => setRoomId(e.target.value)} value={roomId} />
+            <input type='text' className='inputBox' placeholder='USERNAME' onChange={(e) => setUsername(e.target.value)} value={username} ></input>
             <button className='btn joinBtn'>Join</button>
             <span className='createInfo'>
-                Don't have a invite, create a <a href='' className='createNewBtn'>new room</a> 
+                Don't have a invite, create a <a onClick={createNewRoom} href='' className='createNewBtn'>new room</a> 
             </span>
         </div>
 
